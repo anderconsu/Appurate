bd6588e20764413dc97193c7f46657e13f5a9ef8
Endika7593 <e.acedo93@gmail.com>
2023-09-20 17:08:14 +0200

    cambio minimo no es nada

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 // import Nosotros from './components/visible/nosotros/nosotros'
 // import Login from './components/visible/login/login'
 // import Unete from './components/visible/unete/unete'
-import Admin from './components/admin/admin'
+// import Admin from './components/admin/admin'
 import Header from './components/header/header'
 // import Mapa from './components/visible/mapa/mapa'
 
