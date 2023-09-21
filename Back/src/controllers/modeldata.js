a4ac9c3dde058ca13d3dc3c012ea19ede23b7298
anderconsu <anderconsu@gmail.com>
2023-09-21 13:20:23 +0200

    modelo de data conectado

--- Back/src/controllers/modeldata.js
+++ Back/src/controllers/modeldata.js
@@ -1,13 +1,16 @@
 const getModelData = async (data) => {
     console.log(data);
     try {
-        let response = await fetch("http://localhost:3000/api/prediction", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
+        let response = await fetch(
+            `${process.env.MODEL_HOST}/prediccion_niños`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            }
+        );
         if (response.ok) {
             let result = await response.json();
             console.log(result);
