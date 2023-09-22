02eaa6a66ec650d3204f90d9c185790dfebca3a0
Endika7593 <e.acedo93@gmail.com>
2023-09-22 18:56:16 +0200

    fuente importada y css de registro mas o menos

--- Front/src/components/privada/registro/registro.jsx
+++ Front/src/components/privada/registro/registro.jsx
@@ -1,6 +1,8 @@
 import { /*React*/ useState } from "react";
 import "./registro.css";
 
+
+
 const Registro = () => {
     const [location, setLocation] = useState("test");
     const [pH, setpH] = useState("");
@@ -76,47 +78,15 @@ const Registro = () => {
 
                     <div className="mapaRegistro">AQUÍ VA EL MAPA</div>
 
-                    {/* <select
-                        id="location"
-                        name="location"
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                        required
-                    >
-                        <option value="" disabled>
-                            Seleccione una localización
-                        </option>
-                        <option value="Punto 1">Punto 1 - Ría de Bilbao</option>
-                        <option value="Punto 2">Punto 2 - Ría de Bilbao</option>
-                        <option value="Punto 3">Punto 3 - Ría de Bilbao</option>
-                    </select> */}
-                </div>
 
-                {/* institución */}
-                {/* <div className="campo">
-                    <label htmlFor="institution">Institución:</label>
-                    <select
-                        id="institution"
-                        name="institution"
-                        value={institution}
-                        onChange={(e) => setInstitution(e.target.value)}
-                        required
-                    >
-                        <option value="" disabled>
-                            Seleccione una institución
-                        </option>
-                        <option value="Institución 1">Institución 1</option>
-                        <option value="Institución 2">Institución 2</option>
-                        <option value="Institución 3">Institución 3</option>
-                       
-                    </select>
-                </div> */}
+                </div>
 
 
                 <div className="muestras">
-                    
+
                     {/* oxígeno (mg/l) */}
-                    <div className="campo">
+                    <div className="oxigeno">
+                        <label htmlFor="02" className="02">02.</label>
                         <label htmlFor="oxigeno">Oxígeno (mg/l):</label>
                         <input
                             type="text"
@@ -130,7 +100,8 @@ const Registro = () => {
                     </div>
 
                     {/* conductividad */}
-                    <div className="campo">
+                    <div className="conductividad">
+                        <label htmlFor="03" className="03">03.</label>
                         <label htmlFor="conductividad">Conductividad:</label>
                         <input
                             type="text"
@@ -144,7 +115,8 @@ const Registro = () => {
                     </div>
 
                     {/* temperatura */}
-                    <div className="campo">
+                    <div className="temperatura">
+                        <label htmlFor="04" className="04">04.</label>
                         <label htmlFor="temperatura">Temperatura:</label>
                         <input
                             type="text"
@@ -158,21 +130,22 @@ const Registro = () => {
                     </div>
 
                     {/* el pH */}
-                    <div className="campo">
+                    <div className="ph">
+                        <label htmlFor="05" className="05">05.</label>
                         <label htmlFor="pH">pH:</label>
                         <input
                             type="text"
                             step="0.01"
-                            id="pH"
+                            id="ph"
                             name="pH"
                             value={pH}
                             onChange={(e) => setpH(e.target.value)}
                             required
                         />
                     </div>
-                <div className="buttonContainer">
-                    <button className="button" type="submit">Enviar</button>
-                </div>
+                    <div className="buttonContainer">
+                        <button className="button" type="submit">Enviar</button>
+                    </div>
                 </div>
 
             </form>
