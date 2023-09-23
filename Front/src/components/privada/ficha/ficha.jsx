b87bb209bfc58c6df9cc36e4e7773afadf79637e
Nacho Prado <fonsvitae.spain@gmail.com>
2023-09-23 11:12:53 +0200

    cambios con ander

--- Front/src/components/privada/ficha/ficha.jsx
+++ Front/src/components/privada/ficha/ficha.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { useState, useEffect } from 'react';
 
-import { hostUrl } from 'hostUrl';
+// import { hostUrl } from 'hostUrl';
 
 const FichaVisualizacion = () => {
   const [data, setData] = useState({});
