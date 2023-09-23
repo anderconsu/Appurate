4ffb9c710ef6ffa391aafa7c23479a5fd88dffee
anderconsu <anderconsu@gmail.com>
2023-09-23 17:46:00 +0200

    attribution

--- Front/src/components/privada/registro/registro.jsx
+++ Front/src/components/privada/registro/registro.jsx
@@ -88,7 +88,7 @@ const Registro = () => {
                                 scrollWheelZoom={true}
                             >
                                 <TileLayer
-                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                    attribution="Gracias a todo el equipo de appúrate"
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                 />
                                 {coordenadas.map((coordenada, index) => (
