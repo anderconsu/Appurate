b86cefb1e3432879e28a4adc80fa64abda8f85b4
anderconsu <anderconsu@gmail.com>
2023-09-23 18:24:09 +0200

    import  missing useContext

--- Front/src/components/privada/registro/registro.jsx
+++ Front/src/components/privada/registro/registro.jsx
@@ -1,4 +1,4 @@
-import { /*React*/ useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./registro.css";
 import PageContext from "../../../context/pageContext";
 
