f861c86395df1c540ea1b8bbeea7f7c8b6f2854d
anderconsu <anderconsu@gmail.com>
2023-09-25 13:20:57 +0200

    unete mini fix

--- Front/src/components/visible/unete/unete.jsx
+++ Front/src/components/visible/unete/unete.jsx
@@ -1,155 +1,137 @@
-// import { useState, useEffect, useContext } from 'react';
-// import PageContext from '../../context/pageContext';
-// import './unete.scss';
-
+import { useState, useEffect, useContext } from "react";
+import PageContext from "../../../context/pageContext";
+import "./unete.scss";
 
 const Unete = () => {
-  const { page, setPage } = useContext(PageContext);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [institution, setInstitution] = useState('');
-  const [aula, setAula] = useState('');
-  const [proffesor, setProffesor] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [error, setError] = useState(null);
-  const hostUrl = import.meta.env.VITE_BACKEND_URL;
-  useEffect(() => {
-    setPage("unete");
-}, []);
+    const { page, setPage } = useContext(PageContext);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [institution, setInstitution] = useState("");
+    const [aula, setAula] = useState("");
+    const [proffesor, setProffesor] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [error, setError] = useState(null);
+    const hostUrl = import.meta.env.VITE_BACKEND_URL;
 
-  const handleUnete = async (e) => {
-    e.preventDefault();
+    const [message, setMessage] = useState("");
 
-    try {
-      const response = await fetch(`${hostUrl}/user/register`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password, institution, aula, proffesor, email, phone }),
-      });
+    useEffect(() => {
+        setPage("unete");
+    }, []);
 
-      if (response.ok) {
-        
-        console.log("Registro completado");
-      } else {
-        
-        setError("Fallo de registro");
-      }
-    } catch (error) {
-      console.error(error);
-      setError("Error, inténtalo más tarde");
-    }
-  }
+    const handleUnete = async (e) => {
+        e.preventDefault();
+        setMessage("");
+        setMessage("Información enviada");
+    };
 
-  return (
-    <>
-        <div className="adminMain">
-            <div className="adminBody">
-                <h2>Registro de Aula</h2>
-                <p>
-                Si todavía no formas parte del proyecto únete al proyecto.
-                </p>
-                <form className="adminForm" onSubmit={handleUnete}>
-                
+    return (
+        <>
+            <div className="adminMain">
+                <div className="adminBody">
+                    <h2>Registro de Aula</h2>
+                    <p>
+                        Si todavía no formas parte del proyecto únete al
+                        proyecto.
+                    </p>
+                    <form className="adminForm" onSubmit={handleUnete}>
+                        {error && <p className="error-message">{error}</p>}
 
-                {error && <p className="error-message">{error}</p>}
+                        <div>
+                            <input
+                                type="text"
+                                className="adminInput"
+                                name="username"
+                                placeholder="Usuario"
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username}
+                            />
+                        </div>
 
-                <div>
-                    <input
-                    type="text"
-                    className="adminInput"
-                    name="username"
-                    placeholder="Usuario"
-                    onChange={(e) => setUsername(e.target.value)}
-                    value={username}
-                    />
+                        <div>
+                            <input
+                                type="password"
+                                className="adminInput"
+                                name="password"
+                                placeholder="Contraseña"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                type="text"
+                                className="adminInput"
+                                name="institution"
+                                placeholder="Colegio"
+                                onChange={(e) => setInstitution(e.target.value)}
+                                value={institution}
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                type="text"
+                                className="adminInput"
+                                name="aula"
+                                placeholder="Aula"
+                                onChange={(e) => setAula(e.target.value)}
+                                value={aula}
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                type="text"
+                                className="adminInput"
+                                name="proffesor"
+                                placeholder="Profesor"
+                                onChange={(e) => setProffesor(e.target.value)}
+                                value={proffesor}
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                type="text"
+                                className="adminInput"
+                                name="email"
+                                placeholder="Email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                type="number"
+                                className="adminInput"
+                                name="phone"
+                                placeholder="Teléfono"
+                                onChange={(e) => setPhone(e.target.value)}
+                                value={phone}
+                            />
+                        </div>
+
+                        <button className="boton1" type="submit">
+                            Registro
+                        </button>
+                        {message && <p className="message">{message}</p>}
+                    </form>
                 </div>
 
-                <div>
-                    <input
-                    type="password"
-                    className="adminInput"
-                    name="password"
-                    placeholder="Contraseña"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
+                <div className="adminAstronauta">
+                    <img
+                        src="./static/admin/unete.png"
+                        alt="unete"
+                        className="unete"
                     />
                 </div>
-
-                <div>
-                    <input
-                    type="text"
-                    className="adminInput"
-                    name="institution"
-                    placeholder="Colegio"
-                    onChange={(e) => setInstitution(e.target.value)}
-                    value={institution}
-                    />
-                </div>
-
-                <div>
-                    <input
-                    type="text"
-                    className="adminInput"
-                    name="aula"
-                    placeholder="Aula"
-                    onChange={(e) => setAula(e.target.value)}
-                    value={aula}
-                    />
-                </div>
-
-                <div>
-                    <input
-                    type="text"
-                    className="adminInput"
-                    name="proffesor"
-                    placeholder="Profesor"
-                    onChange={(e) => setProffesor(e.target.value)}
-                    value={proffesor}
-                    />
-                </div>
-
-                <div>
-                    <input
-                    type="text"
-                    className="adminInput"
-                    name="email"
-                    placeholder="Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    />
-                </div>
-
-                <div>
-                    <input
-                    type="number"
-                    className="adminInput"
-                    name="phone"
-                    placeholder="Teléfono"
-                    onChange={(e) => setPhone(e.target.value)}
-                    value={phone}
-                    />
-                </div>
-
-                <button className="boton1" type="submit">
-                    Registro
-                </button>
-                </form>
             </div>
-
-            <div className="adminAstronauta">
-                <img
-                    src="./static/admin/unete.png"
-                    alt="unete"
-                    className="unete"
-                />
-            </div>
-            
-        </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Unete;
