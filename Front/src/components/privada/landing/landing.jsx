59ec5e57263e1ef4acacb1ef62218e652c241633
anderconsu <anderconsu@gmail.com>
2023-09-25 19:11:34 +0200

    menu to landing

--- Front/src/components/privada/landing/landing.jsx
+++ Front/src/components/privada/landing/landing.jsx
@@ -3,10 +3,13 @@ import "./landing.scss";
 import { useContext, useEffect } from "react";
 import PageContext from "../../../context/pageContext";
 import { useNavigate } from "react-router-dom";
+
+import Menu from "../menu/menu";
+
 const Landing = () => {
     const { page, setPage } = useContext(PageContext);
     const navigate = useNavigate();
-    const hostUrl = import.meta.env.VITE_BACKEND_URL
+    const hostUrl = import.meta.env.VITE_BACKEND_URL;
 
     const checkAuth = async () => {
         const token = localStorage.getItem("token");
@@ -45,16 +48,17 @@ const Landing = () => {
         setPage("landing");
     }, []);
     return (
-        <>
+        <main className="paginaLanding">
+            <Menu />
             <div className="landingMain">
-
                 <div className="landingBienvenides">
                     <div className="landingBody">
-                        <h2 className="bienvenides">
-                        ¡BIENVENIDES A TODES!
-                        </h2><br />
+                        <h2 className="bienvenides">¡BIENVENIDES A TODES!</h2>
+                        <br />
                         <p className="bienvenides2">
-                        Aquí encontrarás los pasos a seguir para usar nuestra app de registro y análisis de calidad del agua.
+                            Aquí encontrarás los pasos a seguir para usar
+                            nuestra app de registro y análisis de calidad del
+                            agua.
                         </p>
                     </div>
                     <img
@@ -62,18 +66,12 @@ const Landing = () => {
                         alt="peli1"
                         className="peli1"
                     />
-                    
                 </div>
                 <img
                     src="./static/landing/mapa.png"
                     alt="mapa"
                     className="mapa"
                 />
-
-                
-                
-               
-                  
 
                 <div className="landingExplicacion">
                     <div className="landingPrimero">
@@ -84,28 +82,46 @@ const Landing = () => {
                         />
                         <div className="primero">
                             <h3>Lo primero</h3>
-                            <p>que debes hacer es tomar una muestra de agua en una ubicación de tu interés dentro del mapa interactivo que puedes encontrar en la parte de arriba. Puedes usar un kit de prueba de calidad del agua que te proporcionaremos o usar tu propio método. 
-
-Una vez que tengas la muestra, debes introducir los datos relevantes en nuestra plataforma web accediendo a la página “Registrar info.” en el menú de la izquierda. Estos datos se almacenarán en nuestra base de datos y podrás acceder a ellos en cualquier momento. Pulsa el botón “Enviar” para terminar el registro de la información.</p>
+                            <p>
+                                que debes hacer es tomar una muestra de agua en
+                                una ubicación de tu interés dentro del mapa
+                                interactivo que puedes encontrar en la parte de
+                                arriba. Puedes usar un kit de prueba de calidad
+                                del agua que te proporcionaremos o usar tu
+                                propio método. Una vez que tengas la muestra,
+                                debes introducir los datos relevantes en nuestra
+                                plataforma web accediendo a la página “Registrar
+                                info.” en el menú de la izquierda. Estos datos
+                                se almacenarán en nuestra base de datos y podrás
+                                acceder a ellos en cualquier momento. Pulsa el
+                                botón “Enviar” para terminar el registro de la
+                                información.
+                            </p>
                         </div>
                     </div>
 
                     <div className="landingSegundo">
                         <div className="segundo">
                             <h3>Lo segundo...</h3>
-                            <p>que debes hacer es visualizar y analizar los datos que has introducido, para ello accede a la página “Visualizar info.” en el menú de la izquierda. 
-
-Nuestra plataforma web te ofrece herramientas de visualización que te permitirán ver los datos en forma de gráficos o tablas. 
-
-Estas herramientas te ayudarán a identificar patrones o tendencias en los datos y a comprender mejor la calidad del agua en tu ciudad</p>
+                            <p>
+                                que debes hacer es visualizar y analizar los
+                                datos que has introducido, para ello accede a la
+                                página “Visualizar info.” en el menú de la
+                                izquierda. Nuestra plataforma web te ofrece
+                                herramientas de visualización que te permitirán
+                                ver los datos en forma de gráficos o tablas.
+                                Estas herramientas te ayudarán a identificar
+                                patrones o tendencias en los datos y a
+                                comprender mejor la calidad del agua en tu
+                                ciudad
+                            </p>
                         </div>
-                        
+
                         <img
                             src="./static/landing/peli3.png"
                             alt="peli3"
                             className="peli3"
                         />
-                        
                     </div>
 
                     <div className="landingTercero">
@@ -116,15 +132,22 @@ Estas herramientas te ayudarán a identificar patrones o tendencias en los datos
                         />
                         <div className="tercero">
                             <h3>Lo tercero...</h3>
-                            <p>que debes hacer es explorar los “Recursos adicionales” que te ofrecemos. Nuestra plataforma web te proporciona recursos educativos, como videos, artículos y actividades prácticas, que te permitirán aprender más sobre el tema de la calidad del agua. 
-
-Estos recursos te explicarán los diferentes tipos de contaminantes que se encuentran comúnmente en el agua y cómo afectan la salud humana.</p>
-
+                            <p>
+                                que debes hacer es explorar los “Recursos
+                                adicionales” que te ofrecemos. Nuestra
+                                plataforma web te proporciona recursos
+                                educativos, como videos, artículos y actividades
+                                prácticas, que te permitirán aprender más sobre
+                                el tema de la calidad del agua. Estos recursos
+                                te explicarán los diferentes tipos de
+                                contaminantes que se encuentran comúnmente en el
+                                agua y cómo afectan la salud humana.
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </main>
     );
 };
 
