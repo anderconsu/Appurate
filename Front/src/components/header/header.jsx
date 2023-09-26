7968c3cd4210032848939b6973051205fa6ec6c3
anderconsu <anderconsu@gmail.com>
2023-09-26 18:04:27 +0200

    header when logged

--- Front/src/components/header/header.jsx
+++ Front/src/components/header/header.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useEffect } from "react";
+import { useContext, useState } from "react";
 import PageContext from "../../context/pageContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, redirect, useNavigate } from "react-router-dom";
 
 import "./header.scss";
 
-const noCargues = ["admin", "login", "ficha", "landing", "recursos", "registro"];
+const noCargues = [
+    "admin",
+    "login",
+    "ficha",
+    "landing",
+    "recursos",
+    "registro",
+];
 
 const Header = () => {
     const { page, setPage } = useContext(PageContext);
     const navigate = useNavigate();
+    const [professor, setProfessor] = useState("");
     console.log(page);
+
+    useEffect(() => {
+        let localprofe = localStorage.getItem("proffesor");
+        if (localprofe) {
+            setProfessor(localprofe);
+        } else {
+            setProfessor("");
+        }
+    }, [page]);
     return (
         <header className="header">
             <picture className="logoContainer">
@@ -24,24 +41,46 @@ const Header = () => {
 
             {!noCargues.includes(page) ? (
                 <div className="botonesHeader">
-                    <button
-                        className="boton3"
-                        onClick={() => navigate("/nosotros")}
-                    >
-                        Sobre Nosotros
-                    </button>
-                    <button
-                        className="boton2"
-                        onClick={() => navigate("/unete")}
-                    >
-                        ¡Únete!
-                    </button>
-                    <button
-                        className="boton1"
-                        onClick={() => navigate("/login")}
-                    >
-                        Entrar
-                    </button>
+                    {professor ? (
+                        <>
+                            <button
+                                className="boton1"
+                                onClick={() => navigate("/landing")}
+                            >
+                                {professor}
+                            </button>
+                            <button
+                                className="boton2"
+                                onClick={() => {
+                                    localStorage.clear();
+                                    window.location.reload();
+                                }}
+                            >
+                                Cerrar Sesión
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <button
+                                className="boton3"
+                                onClick={() => navigate("/nosotros")}
+                            >
+                                Sobre Nosotros
+                            </button>
+                            <button
+                                className="boton2"
+                                onClick={() => navigate("/unete")}
+                            >
+                                ¡Únete!
+                            </button>
+                            <button
+                                className="boton1"
+                                onClick={() => navigate("/login")}
+                            >
+                                Entrar
+                            </button>
+                        </>
+                    )}
                 </div>
             ) : (
                 <></>
