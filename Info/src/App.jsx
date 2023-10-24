5c5903cefe2432b0959a4155f10b5a573127e1e9
Endika7593 <e.acedo93@gmail.com>
2023-10-24 12:56:08 +0200

    footer importado

--- Info/src/App.jsx
+++ Info/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 function App() {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
     );
 }
---
+++ Info/src/components/footer/footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useContext } from "react";
+import PageContext from "../../context/pageContext";
+
+import "./footer.scss";
+
+const noCargues = ["admin", "login", "unete" ];
+const Footer = () => {
+    const { page, setPage } = useContext(PageContext);
+    return (
+        <div className="footer">
+            {!noCargues.includes(page) ? (
+                <img
+                    src="./static/footer.png"
+                    alt="footer"
+                    className="footerimg"
+                />
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+};
+
+export default Footer;
