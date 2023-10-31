0157d6b0a8e45a2b70c2fe748be7b939379479b5
anderconsu <anderconsu@gmail.com>
2023-10-31 10:16:03 +0100

    current page location component added

--- Info/src/App.jsx
+++ Info/src/App.jsx
@@ -4,11 +4,12 @@ import "./components/general.scss";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar.jsx";
-
+import Current from "./components/current/current";
 function App() {
     return (
         <>
             <Header />
+            <Current />
             <Outlet />
             <Navbar />
             <Footer />
