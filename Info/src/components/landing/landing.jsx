4856d0d5362868810a05d95d34e296f673447999
anderconsu <anderconsu@gmail.com>
2023-11-03 17:06:55 +0100

    pincha aquí fixed

--- Info/src/components/landing/landing.jsx
+++ Info/src/components/landing/landing.jsx
@@ -75,48 +75,51 @@ const Landing = () => {
                         </h2>
                         <ol>
                             <li>
-                                <strong>Falta de Data:</strong> Se identificó una carencia
-                                significativa de datos, lo que representa un
-                                obstáculo importante para comprender y abordar de
-                                manera efectiva la problemática del agua.
-                            </li> 
-                            <li>
-                                <strong>Necesidad
-                                de Implicación de la Sociedad:</strong> Se destacó la
-                                necesidad de involucrar a la sociedad en la
-                                problemática del agua, reconociendo que la
-                                participación y la concienciación de la comunidad
-                                son esenciales para abordar este desafío de manera
-                                integral.
+                                <strong>Falta de Data:</strong> Se identificó
+                                una carencia significativa de datos, lo que
+                                representa un obstáculo importante para
+                                comprender y abordar de manera efectiva la
+                                problemática del agua.
                             </li>
                             <li>
-                                <strong>Concienciación de las Nuevas Generaciones: </strong>
+                                <strong>
+                                    Necesidad de Implicación de la Sociedad:
+                                </strong>{" "}
+                                Se destacó la necesidad de involucrar a la
+                                sociedad en la problemática del agua,
+                                reconociendo que la participación y la
+                                concienciación de la comunidad son esenciales
+                                para abordar este desafío de manera integral.
+                            </li>
+                            <li>
+                                <strong>
+                                    Concienciación de las Nuevas Generaciones:{" "}
+                                </strong>
                                 Se subrayó la importancia de sensibilizar a las
-                                generaciones más jóvenes sobre esta problemática, ya
-                                que representan el futuro y pueden desempeñar un
-                                papel fundamental en la promoción de soluciones
-                                sostenibles.
+                                generaciones más jóvenes sobre esta
+                                problemática, ya que representan el futuro y
+                                pueden desempeñar un papel fundamental en la
+                                promoción de soluciones sostenibles.
                             </li>
-                            
-                             
-                         </ol>
-                         <p>
-                            Estas conclusiones son fundamentales
-                            para orientar nuestras acciones y estrategias
-                            futuras en el contexto de este desafío crucial
-                            propuesto por Aquadat.
-                         </p>
+                        </ol>
+                        <p>
+                            Estas conclusiones son fundamentales para orientar
+                            nuestras acciones y estrategias futuras en el
+                            contexto de este desafío crucial propuesto por
+                            Aquadat.
+                        </p>
                     </div>
                 </section>
             </section>
             <section className="gris2Landing">
                 <h2>A partir de ese punto.</h2>
                 <p>
-                    Nuestro objetivo principal fue desarrollar una <strong>solución
-                    dinámica y educativa</strong>. Tras haber definido claramente el
-                    problema, fuimos capaces de identificar rápidamente una
-                    solución que <strong>involucraría a las generaciones más jóvenes</strong>,
-                    proporcionando un feedback valioso y necesario.
+                    Nuestro objetivo principal fue desarrollar una{" "}
+                    <strong>solución dinámica y educativa</strong>. Tras haber
+                    definido claramente el problema, fuimos capaces de
+                    identificar rápidamente una solución que{" "}
+                    <strong>involucraría a las generaciones más jóvenes</strong>
+                    , proporcionando un feedback valioso y necesario.
                 </p>
             </section>
             <div className="blanco2landing">
@@ -125,25 +128,32 @@ const Landing = () => {
                         Cuando se planteó la creación de una plataforma
                         educativa en la que los estudiantes no solo formarían
                         parte activa del proceso de aprendizaje, sino que
-                        también serían <strong>actores fundamentales en la toma y
-                        análisis de muestras</strong>, se consideró la necesidad de
-                        diseñar una aplicación web que permitiera su uso tanto
-                        en versiones web y tablet, especialmente para el
-                        aprendizaje en el aula mediante proyectores o pantallas.
-                        No obstante, también se reconoció la importancia de
-                        contar con una <strong>versión móvil</strong>, que permitiera la
+                        también serían{" "}
+                        <strong>
+                            actores fundamentales en la toma y análisis de
+                            muestras
+                        </strong>
+                        , se consideró la necesidad de diseñar una aplicación
+                        web que permitiera su uso tanto en versiones web y
+                        tablet, especialmente para el aprendizaje en el aula
+                        mediante proyectores o pantallas. No obstante, también
+                        se reconoció la importancia de contar con una{" "}
+                        <strong>versión móvil</strong>, que permitiera la
                         introducción de datos en el lugar de recogida y la
                         rápida visualización de los resultados.
-                    <br /><br />
-                        Esta versión
-                        móvil se concibió para <strong>garantizar que cualquier persona
-                        interesada en conocer la calidad del agua de la Ría de
-                        Bilbao pudiera acceder de manera sencilla y práctica a
-                        la información relevante</strong>, sin importar su ubicación o
-                        dispositivo de preferencia. De esta manera, se buscó
-                        hacer que la plataforma fuera accesible y efectiva para
-                        todos los usuarios interesados en el estado del agua en
-                        la Ría.
+                        <br />
+                        <br />
+                        Esta versión móvil se concibió para{" "}
+                        <strong>
+                            garantizar que cualquier persona interesada en
+                            conocer la calidad del agua de la Ría de Bilbao
+                            pudiera acceder de manera sencilla y práctica a la
+                            información relevante
+                        </strong>
+                        , sin importar su ubicación o dispositivo de
+                        preferencia. De esta manera, se buscó hacer que la
+                        plataforma fuera accesible y efectiva para todos los
+                        usuarios interesados en el estado del agua en la Ría.
                     </p>
                 </div>
                 <div>
@@ -153,90 +163,87 @@ const Landing = () => {
                         className="app"
                     />
                 </div>
-                
             </div>
             <div className="disenoSitemap">
                 <h2>
-                    Luego de estas consideraciones, nos adentramos en el
-                    diseño del sitemap, donde identificamos tres componentes
+                    Luego de estas consideraciones, nos adentramos en el diseño
+                    del sitemap, donde identificamos tres componentes
                     fundamentales o bloques clave para la webapp.
                 </h2>
             </div>
 
-                <div className="orden">
-                    <div className="unoDos">
-                        <div className="uno">
-                            <div>
-                                <img
-                                    src="./static/landing/uno.png"
-                                    alt="uno"
-                                    className="unoImage"
-                                />
-                            </div>
-                            <p>
-                                El primer bloque se enfoca en la parte pública y
-                                visible de la plataforma. Aquí se incluirá un
-                                mapa accesible para todos los usuarios, que les
-                                permitirá identificar los puntos en la Ría donde
-                                se pueden llevar a cabo actividades
-                                sociodeportivas, gracias a las recomendaciones
-                                de calidad del agua. Además, en esta sección,
-                                los visitantes podrán obtener información
-                                adicional sobre el proyecto y conocer a los
-                                participantes involucrados.
-                            </p>
+            <div className="orden">
+                <div className="unoDos">
+                    <div className="uno">
+                        <div>
+                            <img
+                                src="./static/landing/uno.png"
+                                alt="uno"
+                                className="unoImage"
+                            />
                         </div>
-                        <div className="dos">
-                            <div>
-                                <img
-                                    src="./static/landing/dos.png"
-                                    alt="dos"
-                                    className="dosImage"
-                                />
-                            </div>
-                            <p>
-                                El segundo bloque principal se centra en la
-                                aplicación privada, diseñada específicamente
-                                para tutores y alumnos. Esta área servirá como
-                                un espacio de aprendizaje relacionado con el
-                                agua y permitirá la introducción de datos de las
-                                muestras recopiladas.
-                            </p>
-                        </div>
+                        <p>
+                            El primer bloque se enfoca en la parte pública y
+                            visible de la plataforma. Aquí se incluirá un mapa
+                            accesible para todos los usuarios, que les permitirá
+                            identificar los puntos en la Ría donde se pueden
+                            llevar a cabo actividades sociodeportivas, gracias a
+                            las recomendaciones de calidad del agua. Además, en
+                            esta sección, los visitantes podrán obtener
+                            información adicional sobre el proyecto y conocer a
+                            los participantes involucrados.
+                        </p>
                     </div>
-
-                    <div className="tres">
-                            <div>
-                                <img
-                                    src="./static/landing/mapa.png"
-                                    alt="mapa"
-                                    className="mapa"
-                                />
-                            </div>
-                            <div className="tresTres">
-                                <div>
-                                    <img
-                                        src="./static/landing/tres.png"
-                                        alt="tres"
-                                        className="tresImage"
-                                    />
-                                </div>
-                                <p>
-                                    El tercer bloque aborda el espacio privado
-                                    destinado a los técnicos de la aplicación. Su
-                                    función será dar de alta a los profesores y
-                                    tutores de los colegios en el sistema.
-                                </p>
-                            </div>
-                        
+                    <div className="dos">
+                        <div>
+                            <img
+                                src="./static/landing/dos.png"
+                                alt="dos"
+                                className="dosImage"
+                            />
+                        </div>
+                        <p>
+                            El segundo bloque principal se centra en la
+                            aplicación privada, diseñada específicamente para
+                            tutores y alumnos. Esta área servirá como un espacio
+                            de aprendizaje relacionado con el agua y permitirá
+                            la introducción de datos de las muestras
+                            recopiladas.
+                        </p>
                     </div>
                 </div>
+
+                <div className="tres">
+                    <div>
+                        <img
+                            src="./static/landing/mapa.png"
+                            alt="mapa"
+                            className="mapa"
+                        />
+                    </div>
+                    <div className="tresTres">
+                        <div>
+                            <img
+                                src="./static/landing/tres.png"
+                                alt="tres"
+                                className="tresImage"
+                            />
+                        </div>
+                        <p>
+                            El tercer bloque aborda el espacio privado destinado
+                            a los técnicos de la aplicación. Su función será dar
+                            de alta a los profesores y tutores de los colegios
+                            en el sistema.
+                        </p>
+                    </div>
+                </div>
+            </div>
             <div className="buttonWeb">
                 <button>
                     <Link to="/">Descubre la web</Link>
                 </button>
             </div>
-            
+
             <div className="gris3landing">
                 <div className="azul">
                     <h2>
@@ -244,7 +251,6 @@ const Landing = () => {
                         parte del equipo:
                     </h2>
                     <div className="bewaters2">
-                        
                         <div className="bewaters">
                             <h3>BEWATERS</h3>
                             <div>
@@ -261,25 +267,27 @@ const Landing = () => {
                                 <p>
                                     Somos Bewaters, un equipo compuesto por ocho
                                     apasionados individuos que nos unimos con un
-                                    objetivo común: abordar la problemática del agua en
-                                    la Ría de Bilbao y llenar los vacíos de datos
-                                    relacionados con esta importante cuestión.
+                                    objetivo común: abordar la problemática del
+                                    agua en la Ría de Bilbao y llenar los vacíos
+                                    de datos relacionados con esta importante
+                                    cuestión.
                                 </p>
                                 <ul>
                                     <li>
                                         Lorem ipsum dolor, sit amet consectetur
-                                        adipisicing elit. Sapiente ab ipsa perferendis
-                                        iure ad corrupti sequi quo corporis.
-                                        Repellendus, cumque.
+                                        adipisicing elit. Sapiente ab ipsa
+                                        perferendis iure ad corrupti sequi quo
+                                        corporis. Repellendus, cumque.
                                     </li>
 
                                     <li>
-                                        Laborum delectus repudiandae facilis commodi
-                                        veniam repellat. Quisquam, voluptas debitis.
+                                        Laborum delectus repudiandae facilis
+                                        commodi veniam repellat. Quisquam,
+                                        voluptas debitis.
                                     </li>
                                     <li>
-                                        Laboriosam, quis magnam? Quosque tandem abutere
-                                        patientiam nostram, Catilina?
+                                        Laboriosam, quis magnam? Quosque tandem
+                                        abutere patientiam nostram, Catilina?
                                     </li>
                                 </ul>
                             </div>
@@ -292,15 +300,11 @@ const Landing = () => {
                             </div>
                         </div>
                     </div>
-                   
                 </div>
                 <div className="buttonPa">
-                    <button>
-                        <Link to="/">Pincha aquí:</Link>
-                    </button>
+                    <p>Pincha aquí:</p>
                 </div>
             </div>
-           
         </main>
     );
 };
