231f7defd18a3467c47939dd702e5e4f14fcfedc
Endika7593 <e.acedo93@gmail.com>
2024-01-31 19:27:03 +0100

    cabecera cambiada ?

--- Info/src/components/header/header.jsx
+++ Info/src/components/header/header.jsx
@@ -3,22 +3,32 @@ import "./header.scss";
 const Header = () => {
     return (
         <header className="header">
-            <picture className="logoContainer">
-                <img
-                    src="./static/logo.png"
-                    alt="Mi Logo"
-                    className="logo"
-                    onClick={() =>
-                        (window.location.href = "https://appurate.org")
-                    }
-                />
-            </picture>
-            <button
-                className="boton1 bold"
-                onClick={() => (window.location.href = "https://appurate.org")}
-            >
-                Descubre la web
-            </button>
+            <div className="cabeceraContainer">
+
+                <picture className="logoContainer">
+                    <img
+                        src="./static/logoheader.png"
+                        alt="Mi Logo"
+                        className="logo"
+                        onClick={() =>
+                            (window.location.href = "https://appurate.org")
+                        }
+                    />
+                </picture>
+
+                <div className="botoneraContainer">
+                    <button className="home">HOME</button>
+                    <button className="comoHicimos">CÓMO LO HICIMOS</button>
+                    <button className="contacto">CONTACTO</button>
+                    <button
+                        className="boton1 bold"
+                        onClick={() => (window.location.href = "https://appurate.org")}
+                    >
+                        PROYECTO APPÚRATE
+                    </button>
+                </div>
+
+            </div>
         </header>
     );
 };
